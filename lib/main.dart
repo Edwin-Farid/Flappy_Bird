09fd17a25bfc1c574cb6e28955a0aef8e5f56e1e
Edwin-Farid <117edwin@gmail.com>
2024-01-06 18:35:25 +0800

    feat: add game over screen & add to main

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:js';
 
 import 'package:flame/game.dart';
 import 'package:flappy_bird/game/flappy_bird_game.dart';
+import 'package:flappy_bird/screens/game_over_screens.dart';
 import 'package:flappy_bird/screens/main_menu_screens.dart';
 import 'package:flutter/material.dart';
 
@@ -12,7 +13,8 @@ void main() {
       game: game,
       initialActiveOverlays: [MainMenuScreen.id],
       overlayBuilderMap: {
-        'mainMenu': (context, _) => MainMenuScreen(game: game)
+        'mainMenu': (context, _) => MainMenuScreen(game: game),
+        'gameOver': (context, _) => GameOverScreen(game: game)
       },
     ),
   );
