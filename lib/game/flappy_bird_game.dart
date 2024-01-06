304df03851353516658d9134b70b12e1ae9850a6
Edwin-Farid <117edwin@gmail.com>
2024-01-06 18:43:17 +0800

    feat: add score in game

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -2,11 +2,13 @@ import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
 import 'package:flappy_bird/game/configuration.dart';
+import 'package:flutter/material.dart';
 
 class FlappyBirdGame extends FlameGame with TapDetector, HasCollisionDetection {
   FlappyBirdGame();
 
   late Bird bird;
+  late TextComponent score;
   Timer interval = Timer(Config.pipeInterval, repeat: true);
   bool isHit = false;
 
@@ -17,9 +19,22 @@ class FlappyBirdGame extends FlameGame with TapDetector, HasCollisionDetection {
       Ground(),
       bird = Bird(),
       PipeGroup(),
+      score = buildScore(),
     ]);
 
     interval.onTick = () => add(PipeGroup());
+  }
+
+  TextComponent buildScore() {
+    return TextComponent(
+      text: 'Score: 0',
+      position: Vector2(size.x / 2, size.y / 2 * 0.2),
+      anchor: Anchor.center,
+      textRenderer: TextPaint(
+        style: const TextStyle(
+            fontSize: 40, fontWeight: FontWeight.bold, fontFamily: 'Game'),
+      ),
+    );
   }
 
   @override
@@ -32,5 +47,7 @@ class FlappyBirdGame extends FlameGame with TapDetector, HasCollisionDetection {
   void update(double dt) {
     super.update(dt);
     interval.update(dt);
+
+    score.text = 'Score: ${bird.score}';
   }
 }
