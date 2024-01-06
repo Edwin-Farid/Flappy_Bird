ae1c9646e08b3d5d309e4c656d1f1f5185a3f43e
Edwin-Farid <117edwin@gmail.com>
2024-01-06 17:54:36 +0800

    feat: add Timer and Pipe Interval

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,14 +1,20 @@
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flappy_bird/game/configuration.dart';
 
 class FlappyBirdGame extends FlameGame {
+  FlappyBirdGame();
+
   late Bird bird;
+  Timer interval = Timer(Config.pipeInterval);
+
   @override
   Future<void> onLoad() async {
     addAll([
       Background(),
       Ground(),
       bird = Bird(),
-      PipeGroud(),
+      PipeGroup(),
     ]);
   }
 }
