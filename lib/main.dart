487de96a91c8dcd674a5e77b76fa79a2d9b25530
Edwin-Farid <117edwin@gmail.com>
2024-01-06 18:15:05 +0800

    feat: add main menu screen and add to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'dart:js';
+
 import 'package:flame/game.dart';
 import 'package:flappy_bird/game/flappy_bird_game.dart';
+import 'package:flappy_bird/screens/main_menu_screens.dart';
 import 'package:flutter/material.dart';
 
 final game = FlappyBirdGame();
 void main() {
   runApp(
-    GameWidget(game: game),
+    GameWidget(
+      game: game,
+      initialActiveOverlays: [MainMenuScreen.id],
+      overlayBuilderMap: {
+        'mainMenu': (context, _) => MainMenuScreen(game: game)
+      },
+    ),
   );
 }
