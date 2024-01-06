07da614c77cc92f1bc41f707fd2829aadd1a2c1e
FATHULLAH <kinta0420>
2024-01-06 17:59:32 +0700

    fix: update bird

--- lib/components/bird.dart
+++ lib/components/bird.dart
@@ -1,6 +1,8 @@
 import 'package:flame/components.dart';
+import 'package:flame/effects.dart';
 import 'package:flappy_bird/game/assets.dart';
 import 'package:flappy_bird/game/bird_movement.dart';
+import 'package:flappy_bird/game/configuration.dart';
 import 'package:flappy_bird/game/flappy_bird_game.dart';
 
 class Bird extends SpriteGroupComponent<BirdMovement> with HasGameRef<FlappyBirdGame> {
@@ -20,5 +22,19 @@ class Bird extends SpriteGroupComponent<BirdMovement> with HasGameRef<FlappyBird
       BirdMovement.up: birdUpFlap,
       BirdMovement.down: birdDownFlap,
     };
+  }
+
+  void fly(){
+    add(
+      MoveByEffect(Vector2(0, Config.gravity), EffectController(duration:0.2, curve: Curves.declarate),onComplete: ()=> current = BirdMovement.down),
+    );
+    current = BirdMovement.up;
+  }
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+
+    position.y += Config.birdVelocity * dt;
   }
 }
