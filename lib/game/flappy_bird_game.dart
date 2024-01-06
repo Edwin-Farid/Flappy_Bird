637dc40466a0565497843ff44dfb12eff1d711df
Edwin-Farid <117edwin@gmail.com>
2024-01-06 17:51:42 +0800

    breaking changes: add pipe position and fix game

--- lib/game/flappy_bird_game.dart
+++ lib/game/flappy_bird_game.dart
@@ -1,11 +1,14 @@
 import 'package:flame/game.dart';
 
 class FlappyBirdGame extends FlameGame {
+  late Bird bird;
   @override
   Future<void> onLoad() async {
     addAll([
       Background(),
       Ground(),
+      bird = Bird(),
+      PipeGroud(),
     ]);
   }
 }
