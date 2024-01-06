ee322e9670f49508d84bd92a2195708c75d5b09f
FATHULLAH <kinta0420>
2024-01-06 16:44:59 +0700

    fix: forgot liibrary

--- lib/components/backgound.dart
+++ lib/components/backgound.dart
@@ -1,3 +1,8 @@
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
+import 'package:flappy_bird/game/assets.dart';
+import 'package:flappy_bird/game/flappy_bird_game.dart';
+
 class Background extends SpriteComponent with HasGameRef<FlappyBirdGame>{
     Background();
 
